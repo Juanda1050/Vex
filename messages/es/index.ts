248773f5dcb6808a6077c47ddb7b69ae279e9af8
Juanda Alejandre <danielalejandre1050@gmail.com>
2026-07-05 22:52:55 -0600

    Add modular i18n messages

---
+++ messages/es/index.ts
@@ -0,0 +1,21 @@
+import common from "./common.json";
+import auth from "./auth.json";
+import nav from "./nav.json";
+import products from "./products.json";
+import inventory from "./inventory.json";
+import sales from "./sales.json";
+import purchases from "./purchases.json";
+import settings from "./settings.json";
+
+const messages = {
+  common,
+  auth,
+  nav,
+  products,
+  inventory,
+  sales,
+  purchases,
+  settings,
+};
+
+export default messages;
